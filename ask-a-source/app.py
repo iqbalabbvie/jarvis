81fbe25cf9250da6272bf7d44cf44abc708cd5a9
Kumar <KUMARSX42@abbvienet.com>
2024-09-04 13:56:45 -0400

    jarvis ui and app.py change update

--- ask-a-source/app.py
+++ ask-a-source/app.py
@@ -1,52 +1,69 @@
-import streamlit as st
+
 import dotenv    # loads our .env file into environment variables
 import os        # reads environment variables
 import pathlib   # navigates the file system and opens files
 import pprint    # for inspecting Iliad response messages
 import requests  # for communicating with the Iliad API
 import json
+import re
+
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from functools import partial
 
 dotenv.load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
 
 ILIAD_KEY = os.getenv("ILIAD_API_KEY")
 ILIAD_URL = "https://api-epic.ir-gateway.abbvienet.com/iliad"
 USER_TOKEN = os.getenv("USER_TOKEN")
 
-
-def process_input(input_text):
-    # Process the input text and return the response
-    response = f"You entered: {input_text}"
-    
-    resp = requests.post(
-    url=f"{ILIAD_URL}/api/v1/sources/my-new-source-admp/rag",
-    headers={"x-api-key": ILIAD_KEY, "x-user-token": USER_TOKEN},
-    json={
+@app.route('/api/post-data', methods=['POST'])
+def post_data():
+    data = request.json
+    user_input = data.get('message')
+    payload = {
       "chat_model": "gpt-4o",
-    #   "messages": [{"role": "user", "content": "what is name of the release pipeline"}],
-    #   "messages": [{"role": "user", "content": "what is name of the build pipeline"}],
-    #   "messages": [{"role": "user", "content": "differnt type of utilities"}],
-    #"messages": [{"role": "user", "content": "which utility class increases bottom border width to 5 pixels"}],
-        # "messages": [{"role": "user", "content": "Which icon class we need to use for Add Location"}],
-        # "messages": [{"role": "user", "content": "Which component we can use for Display progress of multiple activities"}],
-        "messages": [{"role": "user", "content": input_text}],
+      "messages": [{"role": "user", "content": user_input}],
       "minimum_score ": 0
-      
     }
-    )
-    pprint.pprint(resp.json())
-    response = resp.json()
-    return response
+    # Process the input text and return the response
+    #response = f"You entered: {user_input}" 
+    try: 
+      resp = requests.post(
+      url=f"{ILIAD_URL}/api/v1/sources/my-new-source-admp/rag",
+      headers={"x-api-key": ILIAD_KEY, "x-user-token": USER_TOKEN},
+      json={
+        "chat_model": "gpt-4o",
+        #   "messages": [{"role": "user", "content": "what is name of the release pipeline"}],
+        #   "messages": [{"role": "user", "content": "what is name of the build pipeline"}],
+        #   "messages": [{"role": "user", "content": "differnt type of utilities"}],
+        #"messages": [{"role": "user", "content": "which utility class increases bottom border width to 5 pixels"}],
+            # "messages": [{"role": "user", "content": "Which icon class we need to use for Add Location"}],
+            # "messages": [{"role": "user", "content": "Which component we can use for Display progress of multiple activities"}],
+          "messages": [{"role": "user", "content": user_input}],
+          "minimum_score ": 0
+          
+        }
+        )
+      pprint.pprint(resp.json())
+      response = resp.json() 
+      
+      #resp = {
+      #  'message': data,
+      #  'items': 'dummy data being passed'
+      #}
+      #pprint.pprint(resp.json())
+      #response = resp.json()
+    except requests.RequestException as e :
+       return jsonify({'response' : 'Error retrieving response'}), 500
+     
 
-def main():
-    st.title("JARVIS")
-    
-    # Get user input
-    input_text = st.text_input("Enter your message...:")
-    # Process the input and display the response
-    if st.button("Process"):
-        response = process_input(input_text)
-        st.write("Response:")
-        st.write(response)
+    return jsonify(response)
+                                  
 
 if __name__ == "__main__":
-    main()
+  app.run(debug=True)
---
+++ ask-a-source/chat-bot-ui/src/App.js
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css'; 
+import Loader from './Loader';
+ 
+function App() {
+  const [message, setMessage] = useState('');
+  const [conversation, setConversation] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [openBladeIndex, setOpenBladeIndex] = useState(null);
+  const [isConfigOpen, setIsConfigOpen] = useState(false);
+  const [userPrefersnce, setUserPreferance] = useState('');
+  const [admpComponent, setAdmpComponent] = useState('accordian');
+ 
+  const handleSend = () => {
+    if (message.trim() === '') return; 
+ 
+    setLoading(true);
+ 
+    // Post the message to the Flask backend
+    axios.post('http://127.0.0.1:5000/api/post-data', { message })
+      .then(response => {
+        let regex = /\[.*?\]/g;
+        let responseText = response.data.content.replace(regex, '');
+        let references = `File Name : ${response.data.references[0].filename}\n Content :\n${response.data.references[0].text}` ;
+        setConversation(prev => [...prev, { user: 'You', text: message}, { user: 'Server', text: responseText, reference: references}]);
+        setMessage('');
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  };
+ 
+  const handleUserPreferanceUpdate = (e) => {
+    setUserPreferance(e.target.value);
+  };
+  
+  const handleAdmpComponentUpdate = (e) => {
+    setAdmpComponent(e.target.value);
+  };
+
+  const handleToggleBlade = (index) => {
+    setOpenBladeIndex(openBladeIndex === index ? null : index);
+  };
+
+  const toggleConfig = () => {
+    setIsConfigOpen(!isConfigOpen);
+  };
+
+  return (
+    <div className="App">
+      <div class="header">
+        <img src= '/header-logo.png' alt = "Logo" className='header-logo' />
+        <h1 className="header-text">ADMP Jarvis</h1>
+        <button onClick={toggleConfig} className='config-toggle-btn'>
+          &#x2699;
+        </button>
+      </div>
+      <div className={`chat-container ${isConfigOpen ? 'config-open' : ''}`}>
+      <div className="chat-box">
+        <div className="messages">
+        {conversation.map((msg, index) => (
+          <div key={index} className={`message ${msg.user === 'You' ? 'user' : 'Server'}`}>
+            <img 
+                src={msg.user === 'You' ? '/user-logo.png' : '/bot-logo.png'}
+                alt={msg.user === 'You' ? 'User' : 'bot'}
+                className='message-logo'
+            />
+            <div className='message-content'>
+              <div>{msg.text}</div>
+              {msg.user === 'Server' && (
+                <>
+                <a
+                  href = '#'
+                  className='reference-link'
+                  onClick={(e) =>{
+                    e.preventDefault();
+                    handleToggleBlade(index);
+                  }}
+                  >
+                      {openBladeIndex === index ? '[-] Collapse' : '[+]Expand Reference'}
+                  </a>
+                  <div className= {`blade ${openBladeIndex === index ? 'open' : 'closed'}`}
+                    dangerouslySetInnerHTML = {{__html: msg.reference.replace(/\n /g, '<br / > ') }}>  
+                  </div>
+                </>
+                
+              )}
+            </div>
+          </div>
+        ))} 
+        {loading && (
+          <div className='messages loader'>
+            <Loader/>
+          </div>
+        )}
+        </div>
+        <div className="input-box">
+        <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder="Type your message..."
+                  />
+        <button onClick={handleSend} disabled={loading}>
+                    {loading ? 'Sending...' : 'Send'}
+        </button>
+        </div>
+      </div>
+      {isConfigOpen && (
+        <div className='config-box'>
+          <div className='config-header'>
+              <span>Settings</span>
+          </div>
+          <div className='admp-component-list'>
+            <span class="admp-component-list-title">ADMP Component List</span>
+            <select value = {admpComponent} onChange={handleAdmpComponentUpdate} >
+              <option value="accordian">Accordian</option>
+              <option value="rangeSlider">Range Slider</option>
+              <option value="icon">Icons</option>
+              <option value="Action">Action</option>
+            </select>
+          </div>
+          <div className='user-preferance'>
+            <span class="user-preferance-title">User Preferance</span>
+            <label>
+              <input type="radio" name="Developer" value="developer" onChange={handleUserPreferanceUpdate} checked={userPrefersnce === "developer"} />
+              Developer
+            </label>
+            <label>
+              <input type="radio" name="Business" value="business" onChange={handleUserPreferanceUpdate}  checked={userPrefersnce === "business"} />
+              Business
+            </label>
+          </div>
+          <button className="save-config-btn">Save</button>
+          <button onClick={toggleConfig} className="close-config-btn">Close</button>
+
+        </div>
+
+      )}
+      {error && <div className="error">Error: {error.message}</div>}
+      </div>     
+    </div>
+);
+}
+ 
+export default App;

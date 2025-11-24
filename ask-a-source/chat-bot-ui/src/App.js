4541dff78868bb9db8c30f325dc12bf939646628
Thirumal N L <thirumal.narayanaswamylogan@abbvie.com>
2025-11-24 15:35:45 +0000

    App.js edited online with Bitbucket

--- ask-a-source/chat-bot-ui/src/App.js
+++ ask-a-source/chat-bot-ui/src/App.js
@@ -19,7 +19,7 @@ function App() {
     setLoading(true);
  
     // Post the message to the Flask backend
-    axios.post('http://jarvis-api-app.aws-k8s-d/api/post-data', { message })
+    axios.post('http://jarvis-api-app.aws-k8s-d.abbvienet.com/api/post-data', { message })
       .then(response => {
         let regex = /\[.*?\]/g;
         let responseText = response.data.content.replace(regex, '');
